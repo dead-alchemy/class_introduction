21187ad70556559195855bc15e982791d0c6cd11
dead-alchemy <dlonicholas@gmail.com>
2023-03-11 19:31:58 -0700

    build.

--- api/index.js
+++ api/index.js
@@ -1,23 +1,22 @@
 const {createExpressApp} = require("./middleware/createExpressApp");
 
 require("dotenv").config({path: "../.env"});
+const path = require("path");
+const express = require("express");
 
 const app = createExpressApp();
 
+const buildPath = path.normalize(path.join(__dirname, "../client/dist"));
+app.use(express.static(buildPath));
 const port = process.env["API_PORT"] || 8080;
 
 // public routes
-app.use("/api/", require("./routes/example.js"));
-app.use("/api/pg", require("./routes/postgres.js"));
-
-app.use((error, req, res, next) => {
-	if (res.headerSent) {
-		return next(error);
-	}
-
-	res.status(error.code || 500);
-	res.json({message: error.message || "An Unknown Error has Occured"});
+app.get("/*", function (req, res) {
+	res.sendFile(path.join(buildPath, "index.html"));
 });
+
+//app.use("/api/", require("./routes/example.js"));
+//app.use("/api/pg", require("./routes/postgres.js"));
 
 app.listen(port, () => {
 	console.log(`api-server listening at http://localhost:${port}`);
